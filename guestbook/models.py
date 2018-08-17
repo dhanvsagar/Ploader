28ba496c2fa0dcf0142eaebd269cac440bf38eca
Dhan V Sagar <dhanvsagar@gmail.com>
2018-08-17 22:03:10 +0530

    Added Image Model

--- guestbook/models.py
+++ guestbook/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Image(models.Model):
+    filename = models.CharField(max_length=30)
+    img_title = models.CharField(max_length=30, default='SOME_STRING')
+    img_desc = models.CharField(max_length=100, null=True)
+    img_tags = models.CharField(max_length=100, null=True)
+    location = models.CharField(max_length=200, default='')

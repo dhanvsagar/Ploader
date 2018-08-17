66fc9531db6db5fdcef0d9347d29f1b1d9704b23
Dhan V Sagar <dhanvsagar@gmail.com>
2018-08-17 22:12:42 +0530

    Added EXIF Model

--- guestbook/models.py
+++ guestbook/models.py
@@ -1,10 +1,22 @@
 from django.db import models
 
-# Create your models here.
 
 class Image(models.Model):
     filename = models.CharField(max_length=30)
-    img_title = models.CharField(max_length=30, default='SOME_STRING')
+    img_title = models.CharField(max_length=30, default='')
     img_desc = models.CharField(max_length=100, null=True)
     img_tags = models.CharField(max_length=100, null=True)
     location = models.CharField(max_length=200, default='')
+
+class EXIF_data(models.Model):
+    exposure_mode   = models.CharField(max_length=10, null=True)
+    focal_length    = models.CharField(max_length=10, null=True)
+    lens            = models.CharField(max_length=10, null=True)
+    apparture       = models.CharField(max_length=10, null=True)
+    metering_mode   = models.CharField(max_length=10, null=True)
+    white_balance   = models.CharField(max_length=10, null=True)
+    make            = models.CharField(max_length=10, null=True)
+    model           = models.CharField(max_length=10, null=True)
+    date_created    = models.DateTimeField(null=True)
+    date_modified   = models.DateTimeField(null=True)
+    resolution      = models.CharField(max_length=10, null=True)

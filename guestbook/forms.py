27989a70423b515eb53007cfec9a52c3f5e13930
Dhan V Sagar <dhanvsagar@gmail.com>
2019-06-27 20:56:37 +0530

    Removed Django files

--- guestbook/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Image
-
-class UploadImageForm(forms.ModelForm):
-    class Meta:
-        model = Image
-        fields = {'document', 'title', 'description', 'photo_type', }
